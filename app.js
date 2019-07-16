dcca06e9edb8a81816598ae8df4eb3ee985f4711
femtioelva <nejfredrika@gmail.com>
2019-07-16 22:41:22 +0200

    add intial routes, ejs

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const expressSanitizer = require('express-sanitizer');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_CONNECTIONSTRING, { useNewUrlParser: true, useFindAndModify: false });
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(expressSanitizer());
+app.use(bodyParser.urlencoded({extended:true})); 
+
+const Blog = mongoose.model('Blog', {
+    title: String,
+    image: String,
+    body: String,
+    createDate: {   type: Date,
+                    default: Date.now }
+});
+
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+})
+
+app.get('/blogs', (req, res) => {
+    Blog.find({}, (error, blogs) => {
+        if(error) {
+            console.log('ERROR. Could not load content: ' + error);
+        } else {
+            res.render('index.ejs', {blogs: blogs});
+        }
+    })
+})
+
+// Form for new post
+app.get('/blogs/new', (req, res) => {
+    res.render('newPost.ejs');
+})
+
+
+// Post new blog post
+app.post('/blogs', (req, res) => {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, (error, newBlog) => {
+        if(error) {
+            console.log('ERROR. Could not create post: ' + error);
+        } else {
+            res.redirect('/blogs/');
+        }
+    })
+})
+
+// Show specific blog post
+app.get('/blogs/:id', (req, res) => {
+    Blog.findById(req.params.id, (error, blogPost) => {
+        if(error) {
+            // Fix blog not found
+            res.redirect('/blogs');
+        } else {
+            res.render('show', {blog: blogPost});
+        }
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log('Blog is up!');
+})
