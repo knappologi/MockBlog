9af9c09469796edf1d13c417796b6117f3eca1ba
billyn <nejfredrika@gmail.com>
2019-09-20 18:04:00 +0200

    create register

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const   passport = require('passport');
+const   passport = require('passport');
 const   User = require('../associations/models/user');
         Post = require('../associations/models/post');
 
@@ -18,6 +18,21 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
   res.render('register');
 });
+
+router.post('/register', (req, res) => {
+  let newUser = new User({username: req.body.user.username});
+  User.register(newUser, req.body.user.password, (err, user) => { // Stores hash!
+      if(err) {
+          console.log('Register new user error: ' + err);
+          return res.render('register', {error: err.message});
+      }
+      passport.authenticate('local')(req, res, ()=> {
+        console.log('New user created: ' + newUser.username);
+          //req.flash('success', 'New user registered.');
+          res.redirect('/posts');
+      })
+  });  
+})
 
 module.exports = router;
 
